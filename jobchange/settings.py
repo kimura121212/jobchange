68448632fc51abd242b4e1eb6c1836ee5ca92114
kimura121212 <yuki.kimura@lion-garden.com>
2019-09-12 01:22:49 +0900

    カスタムユーザー作成

--- jobchange/settings.py
+++ jobchange/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'register',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# registerアプリケーション
+AUTH_USER_MODEL = 'register.User'
