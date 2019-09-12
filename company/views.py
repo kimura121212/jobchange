e17f846830b3687f5237ae42f5a710aae5bde829
kimura121212 <yuki.kimura@lion-garden.com>
2019-09-12 19:06:29 +0900

    不要ライブラリ削除

--- company/views.py
+++ company/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.sites.shortcuts import get_current_site
-from django.core.signing import BadSignature, SignatureExpired, loads, dumps
-from django.http import Http404, HttpResponseBadRequest
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
-from django.template.loader import render_to_string
 from django.views import generic
 from .models import Company
 from .forms import CompanyRegisterForm, CompanyUpdateForm
